e1cb2719824b33f2e2981aa93931fe726eef2e8d
Nikhil-good <Nikhiludc06@gmail.com>
2025-07-01 03:14:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/unique-design-consultant/', // ✅ Your repo name here
   plugins: [react()],
-  base: '/udc/', // 👈 IMPORTANT: replace with your actual GitHub repo name
-
 })
